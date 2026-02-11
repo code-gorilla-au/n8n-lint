29ad0693d2a92cb367296ad5a5dd55df57defc1d
frag223 <m.riquelme223@gmail.com>
2026-02-12 07:33:49 +1100

    update rule and template

--- internal/rules/rule_no_infinite_loop.go
+++ internal/rules/rule_no_infinite_loop.go
@@ -1,5 +1,7 @@
 package rules
 
+import "github.com/code-gorilla-au/n8n-lint/internal/n8n"
+
 const (
 	ruleNameNoInfiniteLoop        = "NO_INFINITE_LOOP"
 	ruleDescriptionNoInfiniteLoop = "TODO"
@@ -9,6 +11,15 @@ var ruleNoInfiniteLoop = Rule{
 	Name:        ruleNameNoInfiniteLoop,
 	Description: ruleDescriptionNoInfiniteLoop,
 	ruleFn: func(finder Finder, config Ruleset) (EvaluationOutcome, error) {
-		return EvaluationOutcome{}, nil
+
+		outcome := EvaluationOutcome{
+			File:            finder.GetFileName(),
+			RuleName:        ruleNameNoInfiniteLoop,
+			RuleDescription: ruleDescriptionNoInfiniteLoop,
+			Nodes:           make([]n8n.Node, 0),
+			Report:          config.NoDeadEnds.ReportLevel(),
+		}
+
+		return outcome, nil
 	},
 }

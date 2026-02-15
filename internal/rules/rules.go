7b85517ad49f7b3ac6a2d8c334140e97c5d66375
frag223 <m.riquelme223@gmail.com>
2026-02-15 20:00:29 +1100

    adding docs

--- internal/rules/rules.go
+++ internal/rules/rules.go
@@ -4,6 +4,7 @@ import (
 	"github.com/code-gorilla-au/n8n-lint/internal/n8n"
 )
 
+// NewRulesEngine initializes and returns a new Engine instance configured with the provided Configuration.
 func NewRulesEngine(config Configuration) Engine {
 	return Engine{
 		config: config,
@@ -11,6 +12,7 @@ func NewRulesEngine(config Configuration) Engine {
 	}
 }
 
+// Run evaluates the provided workflow against all rules and returns a FileReport summarizing the outcomes or an error.
 func (e Engine) Run(workflow n8n.Workflow) (FileReport, error) {
 	finder := n8n.NewWorkflowTree(workflow)
 
@@ -29,6 +31,7 @@ func (e Engine) Run(workflow n8n.Workflow) (FileReport, error) {
 	return NewReport(outcomes), nil
 }
 
+// Run evaluates the rule against the provided Finder and Ruleset, returning an EvaluationOutcome or an error if it fails.
 func (r Rule) Run(finder Finder, config Ruleset) (EvaluationOutcome, error) {
 	return r.ruleFn(finder, config)
 }
